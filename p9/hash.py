8d2e6fe66147d5653cda3b3f0967fd259f0c63e1
cejnik <chejnma@gmail.com>
2026-03-02 18:08:55 +0100

    bcrypt

--- p9/hash.py
+++ p9/hash.py
@@ -1,6 +1,13 @@
 import bcrypt
 
+# Registrace
 password = b'admin'
 hash = bcrypt.hashpw(password, bcrypt.gensalt())
 
-print(hash)
+# Přihlášení
+user_password = bytes(input('Napište své heslo: '), encoding='utf-8')
+
+if bcrypt.checkpw(user_password, hash):
+    print("Úspěšné přihlášení")
+else:
+    print("Špatně zadané heslo")
